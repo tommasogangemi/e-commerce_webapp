e8748a52340461addeeafd678ccdf2b054f8be13
tommasogangemi <gangemi.tommaso@gmail.com>
2020-11-17 17:14:12 +0100

    added routing infrastructure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import './App.css';
+
+function HatsPage() {
+	return (
+		<div>
+			<h1>HATS</h1>
+		</div>
+	);
+}
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<Switch>
+				<Route exact path='/' component={Homepage} />
+				<Route path='/shop/hats' component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
