162c66860204bd4e9feba514383a95a6422a8010
tommasogangemi <gangemi.tommaso@gmail.com>
2020-11-18 11:19:22 +0100

    added shop page and preview gallery

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
+import ShopPage from './pages/shop/shop';
 import './App.css';
-
-function HatsPage() {
-	return (
-		<div>
-			<h1>HATS</h1>
-		</div>
-	);
-}
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path='/' component={Homepage} />
-				<Route path='/shop/hats' component={HatsPage} />
+				<Route path='/shop/' component={ShopPage} />
 			</Switch>
 		</div>
 	);
