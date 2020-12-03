701498b2c3a02dcbb29f756313443203b1e0f626
tommasogangemi <gangemi.tommaso@gmail.com>
2020-12-03 11:23:03 +0100

    added chunk optimization: lazy components, Suspence, and Error Boundaries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-//funtionality
-import React, { useEffect } from 'react';
+//functionality
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 //components
-import Homepage from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop';
-import CheckoutPage from './pages/checkout/checkout';
-import SigninAndSignup from './pages/signin-and-signup/signin-and-signup';
 import Header from './components/header/header';
+import Spinner from './components/spinner/spinner';
+import ErrorBoundary from './components/error-boundary/error-boundary';
 //redux
 import { connect } from 'react-redux';
 //styles
@@ -16,12 +14,14 @@ import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user-selectors';
 import { checkUserSession } from './redux/user/user-actions';
 
-const App = ({ currentUser, checkUserSession }) => {
-	// componentDidMount() {
-	// 	const { checkUserSession } = this.props;
-	// 	checkUserSession();
-	// }
+const Homepage = lazy(() => import('./pages/homepage/homepage'));
+const ShopPage = lazy(() => import('./pages/shop/shop'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout'));
+const SigninAndSignup = lazy(() =>
+	import('./pages/signin-and-signup/signin-and-signup')
+);
 
+const App = ({ currentUser, checkUserSession }) => {
 	useEffect(() => {
 		checkUserSession();
 	}, [checkUserSession]);
@@ -30,16 +30,20 @@ const App = ({ currentUser, checkUserSession }) => {
 		<div>
 			<Header />
 			<Switch>
-				<Route exact path='/' component={Homepage} />
-				<Route path='/shop' component={ShopPage} />
-				<Route exact path='/checkout' component={CheckoutPage} />
-				<Route
-					exact
-					path='/signin'
-					render={() =>
-						currentUser ? <Redirect to='/' /> : <SigninAndSignup />
-					}
-				/>
+				<ErrorBoundary>
+					<Suspense fallback={<Spinner />}>
+						<Route exact path='/' component={Homepage} />
+						<Route path='/shop' component={ShopPage} />
+						<Route exact path='/checkout' component={CheckoutPage} />
+						<Route
+							exact
+							path='/signin'
+							render={() =>
+								currentUser ? <Redirect to='/' /> : <SigninAndSignup />
+							}
+						/>
+					</Suspense>
+				</ErrorBoundary>
 			</Switch>
 		</div>
 	);
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 //components
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview-container';
-import CollectionContainer from '../collections/collection-container';
+import Spinner from '../../components/spinner/spinner';
+import ErrorBoundary from '../../components/error-boundary/error-boundary';
 //other
 import { fetchCollectionsStart } from '../../redux/shop/shop-actions';
 
-const ShopPage = ({ fetchCollectionsStart, match }) => {
-	// componentDidMount() {
-	// 	const { fetchCollectionsStart } = this.props;
-	// 	fetchCollectionsStart();
-	// }
+const CollectionsOverviewContainer = lazy(() =>
+	import('../../components/collections-overview/collections-overview-container')
+);
+const CollectionContainer = lazy(() =>
+	import('../collections/collection-container')
+);
 
+const ShopPage = ({ fetchCollectionsStart, match }) => {
 	useEffect(() => {
 		fetchCollectionsStart();
 	}, [fetchCollectionsStart]);
 
 	return (
 		<div className='shop-page'>
-			<Route
-				exact
-				path={`${match.path}`}
-				component={CollectionsOverviewContainer}
-			/>
-			<Route
-				path={`${match.path}/:collectionParams`}
-				component={CollectionContainer}
-			/>
+			<ErrorBoundary>
+				<Suspense fallback={<Spinner />}>
+					<Route
+						exact
+						path={`${match.path}`}
+						component={CollectionsOverviewContainer}
+					/>
+					<Route
+						path={`${match.path}/:collectionParams`}
+						component={CollectionContainer}
+					/>
+				</Suspense>
+			</ErrorBoundary>
 		</div>
 	);
 };
